62762866b30434b8c0e964b82289c6c8d46a3c4e
ajs00016@red.ujaen.es <ambrosio24>
2018-01-07 19:43:26 +0100

    Corrigiendo fallos de nombre y añadiendo hebra.

--- app/src/main/java/es/ujaen/git/ssmm1718_g06_practica2/Registro.java
+++ app/src/main/java/es/ujaen/git/ssmm1718_g06_practica2/Registro.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
-    //Declaramos los edittext para nuestro formulario de publicación de anuncio.
+    //Declaramos los EditText (y el botón) para nuestro formulario de publicación de anuncio.
     EditText edtId,edtNombre,edtEmpresa,edtDescripcion;
     Button btnAgregar;
 
@@ -18,25 +18,41 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        edtId=(EditText)findViewById(R.id.edtID);
-        edtNombre=(EditText)findViewById(R.id.edtNombre);
-        edtEmpresa=(EditText)findViewById(R.id.edtEmpresa);
-        edtDescripcion=(EditText)findViewById(R.id.edtDescripcion);
+//Creamos un método para crear la nueva hebra de trabajo de la actividad.
+        nuevaHebra();
 
-        btnAgregar=(Button)findViewById(R.id.btnAgregar);
+    }
 
-        //Ahora, creamos una instancia de nuestra clase creada anteriormente para así poder
-        //hacer uso de los métodos de esta clase y mantener nuestra BD.
+    private void nuevaHebra() {
+        //Creamos una instancia de la clase Thread junto a un objeto Runnable.
 
-        final AnuncioBD anuncioBD = new AnuncioBD(getApplicationContext());
-
-        btnAgregar.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
             @Override
+            public void run() {
+                //Lugar dónde realizar el proceso.
+                edtId=(EditText)findViewById(R.id.edtID);
+                edtNombre=(EditText)findViewById(R.id.edtNombre);
+                edtEmpresa=(EditText)findViewById(R.id.edtEmpresa);
+                edtDescripcion=(EditText)findViewById(R.id.edtDescripcion);
 
-            public void onClick(View view){
-                anuncioBD.agregarAnuncio(edtId.getText().toString(),edtNombre.getText().toString(),edtEmpresa.getText().toString(),edtDescripcion.getText().toString());
-                Toast.makeText(getApplicationContext(),"SE AGREGÓ CORRECTAMENTE",Toast.LENGTH_SHORT).show();
+                btnAgregar=(Button)findViewById(R.id.btnAgregar);
+
+                //Ahora, creamos una instancia de nuestra clase creada anteriormente para así poder
+                //hacer uso de los métodos de esta clase y mantener nuestra BD.
+
+                final AnuncioBD anuncioBD = new AnuncioBD(getApplicationContext());
+
+                btnAgregar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+
+                    public void onClick(View view){
+                        anuncioBD.agregarAnuncio(edtId.getText().toString(),edtNombre.getText().toString(),edtEmpresa.getText().toString(),edtDescripcion.getText().toString());
+                        Toast.makeText(getApplicationContext(),"SE AGREGÓ CORRECTAMENTE",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
-        });
+        }).start();
     }
 }
