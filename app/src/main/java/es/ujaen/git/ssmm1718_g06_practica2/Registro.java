5ca07250545dd78ecd70b0eb1ee911eae8c14f1e
ajs00016@red.ujaen.es <ambrosio24>
2018-01-11 21:59:16 +0100

    Añadido cambios a hebra.

--- app/src/main/java/es/ujaen/git/ssmm1718_g06_practica2/Registro.java
+++ app/src/main/java/es/ujaen/git/ssmm1718_g06_practica2/Registro.java
@@ -8,6 +8,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+
+  /********CÓDIGO ANTERIOR A TUTORÍA. ********
+
     //Declaramos los EditText (y el botón) para nuestro formulario de publicación de anuncio.
     EditText edtId,edtNombre,edtEmpresa,edtDescripcion;
     Button btnAgregar;
@@ -60,4 +63,73 @@ public class Registro extends AppCompatActivity {
             }
         }).start();
     }
+    */
+  // ********CÓDIGO TUTORÍA********
+
+  //Declaramos los EditText (y el botón) para nuestro formulario de publicación de anuncio.
+  EditText edtId,edtNombre,edtEmpresa,edtDescripcion;
+    Button btnAgregar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+//Asociamos con los elementos de nuestro formulario de publicación de anuncio.
+        edtId=(EditText)findViewById(R.id.edtID);
+        edtNombre=(EditText)findViewById(R.id.edtNombre);
+        edtEmpresa=(EditText)findViewById(R.id.edtEmpresa);
+        edtDescripcion=(EditText)findViewById(R.id.edtDescripcion);
+
+        btnAgregar=(Button)findViewById(R.id.btnAgregar);
+
+//Creamos un método para crear la nueva hebra de trabajo de la actividad.
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view){
+
+                nuevaHebra();
+            }
+        });
+        // nuevaHebra();
+
+    }
+
+    private void nuevaHebra() {
+        //Creamos una instancia de la clase Thread junto a un objeto Runnable.
+        final String id=edtId.getText().toString();
+        final String nombre=edtNombre.getText().toString();
+        final String empresa=edtEmpresa.getText().toString();
+        final String descripcion=edtDescripcion.getText().toString();
+        //,edtNombre.getText().toString(),edtEmpresa.getText().toString(),edtDescripcion.getText().toString()
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Lugar dónde realizar el proceso.
+
+                //Ahora, creamos una instancia de nuestra clase creada anteriormente para así poder
+                //hacer uso de los métodos de esta clase y mantener nuestra BD.
+
+                //Creamos una instancia de la clase anuncioBD.
+
+                final AnuncioBD anuncioBD = new AnuncioBD(getApplicationContext());
+
+                //Llamamos a agregarAnuncio.
+                anuncioBD.agregarAnuncio(id,nombre,empresa,descripcion);
+                //Verificación.
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),"SE AGREGÓ CORRECTAMENTE",Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                anuncioBD.close();
+
+            }
+        }).start();
+    }
+
+
 }
